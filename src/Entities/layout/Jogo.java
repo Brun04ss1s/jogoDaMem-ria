9911354a02b9f7d631f70c4b5f3da11e719cda04
Bruno Gabriel <brunofigueiredoassis@outlook.com>
2024-11-09 22:14:54 -0300

    Primeira Versão

---
+++ src/Entities/layout/Jogo.java
@@ -0,0 +1,192 @@
+package Entities.layout;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Jogo extends JFrame {
+    int pontuacao = 100;
+    private Font barraFonte = new Font("Arial Black", Font.BOLD,10);
+    private Font jogoFonte = new Font("Arial Black", Font.BOLD, 26);
+    private JToolBar barraDeFerramenta = new JToolBar();
+    private JButton menuIniciar = new JButton("Menu");
+    private JButton reiniciarJogo = new JButton("Reiniciar");
+    private JButton estatistica = new JButton("Estátisticas");
+    private JButton[] cards = new JButton[12];
+    private GridLayout layout = new GridLayout(4,3);
+    private JPanel grid = new JPanel();
+    private JPanel barraPontos = new JPanel();
+    private JLabel pontuacaoJogador = new JLabel();
+    private List<Integer> valoresCartas = new ArrayList<>();
+    private int primeiroBotao = 0;
+    private int segundoBotao = 0;
+    private int numeroDeCliques = 0, quantidade_acertos = 0;
+
+    public Jogo(){
+        super();
+        configurarTelaJogo();
+        iniciarJogo();
+    }
+
+    private void configurarTelaJogo(){
+        setTitle("JOGO DA MEMÓRIA");
+        setSize(new Dimension(700, 500));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+    private void iniciarJogo(){
+        //Criar a barra do menu
+        menuIniciar.setFont(barraFonte);
+        reiniciarJogo.setFont(barraFonte);
+        estatistica.setFont(barraFonte);
+        barraDeFerramenta.add(menuIniciar);
+        barraDeFerramenta.add(reiniciarJogo);
+        barraDeFerramenta.add(estatistica);
+        add(barraDeFerramenta,BorderLayout.NORTH);
+        pontuacaoJogador.setFont(barraFonte);
+        pontuacaoJogador.setText("Pontos: " + pontuacao);
+        barraPontos.add(pontuacaoJogador);
+        add(barraPontos,BorderLayout.SOUTH);
+
+        valoresCartas = gerarValores();
+        //Criar Cards na tela
+        for(int i = 0; i < 12; i++){
+            int posicao = i;
+            cards[i] = new JButton();
+            grid.add(cards[i]);
+            cards[i].setFont(jogoFonte);
+            cards[i].setVisible(true);
+
+            int valorBotao = valoresCartas.get(i);
+            cards[i].putClientProperty("valor",valorBotao);
+
+            cards[i].setText("");
+
+            cards[i].addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    JButton cliqueBotao = (JButton) e.getSource();
+                    int value = (int) cliqueBotao.getClientProperty("valor");
+                    cliqueBotao.setText(String.valueOf(value));
+                    cliqueBotao.setEnabled(false);
+
+                    numeroDeCliques++;
+
+                    if(numeroDeCliques == 1){
+                        primeiroBotao = posicao;
+                    }
+                    if(numeroDeCliques == 2){
+                        segundoBotao = posicao;
+                        verificarValores();
+                    }
+                }
+            });
+        }
+
+        grid.setLayout(layout);
+        add(grid, BorderLayout.CENTER);
+
+        //Ação do botão menu
+        menuIniciar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                voltarMenu();
+            }
+        });
+
+        //Ação do botão reiniciar
+        reiniciarJogo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                reiniciarJogo();
+            }
+        });
+
+    }
+    public void voltarMenu(){
+        int retorno = configuracaoTelaConfirmacao("Deseja voltar ao menu?");
+
+        if(retorno == 0){
+            this.dispose();
+            SwingUtilities.invokeLater(() -> {
+                Menu mainFrame = new Menu();
+                mainFrame.setVisible(true);
+            });
+        }
+    }
+    public List<Integer> gerarValores(){
+        List<Integer> valor = new ArrayList<>();
+
+        for(int i = 0; i < 6; i++){
+            valor.add(i);
+            valor.add(i);
+        }
+        Collections.shuffle(valor);
+        return valor;
+    }
+    public void verificarFimDoJogo() {
+        if (quantidade_acertos == 6) {
+            exibirTelaFim("Parabéns, você venceu! Deseja voltar ao menu ou reiniciar o jogo?");
+        } else if (pontuacao <= 0) {
+            exibirTelaFim("Fim de jogo! Você perdeu. Deseja voltar ao menu ou reiniciar o jogo?");
+        }
+    }
+    public void exibirTelaFim(String mensagem) {
+        int resposta = JOptionPane.showOptionDialog(
+                this,
+                mensagem,
+                "Fim de Jogo",
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.INFORMATION_MESSAGE,
+                null,
+                new String[]{"Voltar ao Menu", "Reiniciar"},
+                "Voltar ao Menu"
+        );
+
+        if (resposta == JOptionPane.YES_OPTION) {
+            voltarMenu();
+        } else if (resposta == JOptionPane.NO_OPTION) {
+            reiniciarJogo();
+        }
+    }
+    public void verificarValores(){
+        if (!cards[primeiroBotao].getClientProperty("valor").equals(cards[segundoBotao].getClientProperty("valor"))) {
+            cards[primeiroBotao].setText("");
+            cards[segundoBotao].setText("");
+            cards[primeiroBotao].setEnabled(true);
+            cards[segundoBotao].setEnabled(true);
+            pontuacao -= 5;
+        } else {
+            pontuacao += 10;
+            quantidade_acertos++;
+        }
+
+        pontuacaoJogador.setText("Pontos: " + pontuacao);
+        numeroDeCliques = 0;
+        primeiroBotao = 0;
+        segundoBotao = 0;
+
+        verificarFimDoJogo();
+    }
+    public void reiniciarJogo(){
+        int retorno = configuracaoTelaConfirmacao("Deseja Reiniciar o jogo?");
+        if(retorno == 0){
+            this.dispose();
+            new Jogo().setVisible(true);
+        }
+    }
+    public static int configuracaoTelaConfirmacao(String mensagem){
+        return JOptionPane.showConfirmDialog(
+                null,
+                mensagem,
+                "Confirmação",
+                JOptionPane.YES_OPTION,
+                JOptionPane.NO_OPTION
+        );
+    }
+}
---
+++ src/Entities/layout/Menu.java
@@ -0,0 +1,95 @@
+package Entities.layout;
+
+import Entities.layout.Jogo;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Menu extends JFrame {
+
+    private JPanel painel;
+    private JLabel texto;
+    private JButton iniciar = new JButton("Iniciar");
+    private JButton estatistica = new JButton("Estatisticas");
+    private JButton fechar = new JButton("Finalizar");
+
+    public Menu() {
+        super();
+        configuracaoFrame();
+        menu();
+        add(painel);
+    }
+
+    public void configuracaoFrame() {
+        setTitle("JOGO DA MEMÓRIA");
+        setSize(new Dimension(700, 500));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+    private void menu() {
+        painel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        //Configuração para aparecer centralizado
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.insets = new Insets(-100,0,30,0);
+
+        //Adicionar o texto do menu
+        texto = new JLabel("JOGO DA MEMÓRIA");
+        texto.setFont(new Font("Arial Black", Font.BOLD, 24));
+        texto.setHorizontalAlignment(SwingConstants.CENTER);
+        painel.add(texto,gbc);
+
+        //Adicionar os botões nas linhas abaixo
+        gbc.gridy = 1;
+        gbc.insets = new Insets(10,0,0,0);
+        iniciar.setPreferredSize(new Dimension(120,40));
+        iniciar.setFont(new Font("Arial Black",Font.BOLD, 12));
+        iniciar.setBackground(Color.green);
+        painel.add(iniciar,gbc);
+
+        gbc.gridy = 2;
+        gbc.insets = new Insets(10,0,0,0);
+        estatistica.setPreferredSize(new Dimension(120,40));
+        estatistica.setFont(new Font("Arial Black", Font.BOLD, 12));
+        painel.add(estatistica,gbc);
+
+        gbc.gridy = 3;
+        gbc.insets = new Insets(10,0,0,0);
+        fechar.setPreferredSize(new Dimension(120,40));
+        fechar.setFont(new Font("Arial Black", Font.BOLD, 12));
+        fechar.setBackground(Color.red);
+        painel.add(fechar,gbc);
+
+        //Adicionar funcionalidade aos botões
+
+        iniciar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                iniciarJogo();
+            }
+        });
+
+        fechar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                fecharJogo();
+            }
+        });
+
+    }
+    private void iniciarJogo(){
+        this.dispose();
+        Jogo jogo = new Jogo();
+        jogo.setVisible(true);
+
+    }
+    private void fecharJogo(){
+        this.dispose();
+    }
+}
